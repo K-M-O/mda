ab414ee60ea7d7d1bffd38b46599412d99ad7b62
K-M-O <itigamer123@gmail.com>
2021-04-28 13:39:30 +0200

    v4.1.2

--- controllers/homeCo.js
+++ controllers/homeCo.js
@@ -7,6 +7,6 @@ const Image = require('../models/image')
 exports.getAll = (req,res)=>{
     res.render('all',{name:req.params.name})
 }
-exports.checkQuery = (req,res,next)=>{
+exports.home = (req,res,next)=>{
     res.render('home')
 }
